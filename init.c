0eb8ae1f959c4e0ee0d819268ebb829a4d39980f
Anastacia <anaanestesia@gmail.com>
2022-10-11 11:11:13 +0100

    corrected data race with 1 philo

--- init.c
+++ init.c
@@ -6,7 +6,7 @@
 /*   By: anastacia <anastacia@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 14:45:35 by anastacia         #+#    #+#             */
-/*   Updated: 2022/10/07 17:46:01 by anastacia        ###   ########.fr       */
+/*   Updated: 2022/10/11 10:58:31 by anastacia        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,10 +71,10 @@ int	create_threads(void)
 	data()->forks = malloc(sizeof(pthread_mutex_t) * data()->nb_philo);
 	if (!philo || !data()->forks)
 		return (0);
+	pthread_mutex_init(&data()->mutex_death, NULL);
 	i = -1;
 	while (++i < data()->nb_philo)
 		pthread_mutex_init(&data()->forks[i], NULL);
-	pthread_mutex_init(&data()->mutex_death, NULL);
 	i = -1;
 	while (++i < data()->nb_philo)
 	{
@@ -86,10 +86,10 @@ int	create_threads(void)
 	i = -1;
 	while (++i < data()->nb_philo)
 		pthread_join(philo[i].tid, NULL);
+	// pthread_mutex_destroy(&data()->mutex_death);
 	i = -1;
 	while (++i < data()->nb_philo)
 		pthread_mutex_destroy(&data()->forks[i]);
-	pthread_mutex_destroy(&data()->mutex_death);
 	free (data()->forks);
 	free (philo);
 	return (0);
