6b56204ce92ba4083ad1ea450d3b78e1056492ea
Anastacia <anaanestesia@gmail.com>
2022-10-14 15:56:06 +0100

    finished!

--- routine.c
+++ routine.c
@@ -6,7 +6,7 @@
 /*   By: anastacia <anastacia@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 15:39:51 by anastacia         #+#    #+#             */
-/*   Updated: 2022/10/14 14:50:09 by anastacia        ###   ########.fr       */
+/*   Updated: 2022/10/14 15:43:59 by anastacia        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,40 +30,33 @@ int	define_forks(t_philo *philo)
 
 int	take_forks(t_philo *philo)
 {
-	if (!philo->finish && !data()->death)
+	if (!philo->left_status && !philo->right_status)
 	{
-		if (!philo->left_status && !philo->right_status)
-		{
-			pthread_mutex_lock(&data()->forks[philo->right]);
-			philo->right_status = true;
-			print(philo, "has taken a fork");
-			if (data()->nb_philo == 1)
-				return (0);
-			pthread_mutex_lock(&data()->forks[philo->left]);
-			philo->left_status = true;
-			print(philo, "has taken a fork");
-		}
-		return (0);
+		pthread_mutex_lock(&data()->forks[philo->right]);
+		philo->right_status = true;
+		print(philo, "has taken a fork");
+		if (data()->nb_philo == 1)
+			return (0);
+		pthread_mutex_lock(&data()->forks[philo->left]);
+		philo->left_status = true;
+		print(philo, "has taken a fork");
 	}
 	return (0);
 }
 
 int	to_eat(t_philo *philo)
 {
-	if (!philo->finish && !data()->death)
+	if (philo->left_status && philo->right_status)
 	{
-		if (philo->left_status && philo->right_status)
+		if (philo->meals == 0 || philo->meals < data()->nb_meals)
 		{
-			if (philo->meals == 0 || philo->meals <= data()->nb_meals)
-			{
-				print(philo, "is eating");
-				philo->last_meal = timer();
-				to_wait(data()->time_to_eat);
-			}
-			if (data()->nb_meals != 0)
-				philo->meals++;
-			check_meals(philo);
+			print(philo, "is eating");
+			philo->last_meal = timer();
+			to_wait(data()->time_to_eat);
 		}
+		if (data()->nb_meals != 0)
+			philo->meals++;
+		check_meals(philo);
 	}
 	return (0);
 }
@@ -85,11 +78,8 @@ int	leave_forks(t_philo *philo)
 
 int	to_sleep_and_think(t_philo *philo)
 {	
-	if (!philo->finish && !data()->death)
-	{
-		print(philo, "is sleeping");
-		to_wait(data()->time_to_sleep);
-		print(philo, "is thinking");
-	}
+	print(philo, "is sleeping");
+	to_wait(data()->time_to_sleep);
+	print(philo, "is thinking");
 	return (0);
 }
